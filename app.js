f9dd2c0d4ef6fb954a7b4942cd79c8d63a42e4d1
Sanskriti Gupta <56277500+SanskritiGupta@users.noreply.github.com>
2021-04-06 16:11:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// declaration
+let searchFeild = document.querySelector(".searchfeild");
+// add eventlistner
+
+// search feild
+searchFeild.addEventListener('mouseenter',function(){
+    searchFeild.setAttribute("placeholder","");
+    
+});
+searchFeild.addEventListener('mouseleave',function(){
+    searchFeild.setAttribute("placeholder","Search your question");
+    
+});
+
+// hover effect on navbar
+
+const blackboard = document.querySelector('.blackboard')
+// const htmltext;
+// htmltext = `
+//     <ul class="hover-effect">
+//         <li>Dashboard</li>
+//         <li>Collaborate</li>
+//         <li>Credentials</li>
+//         <li>Notification</li>
+//     </ul>`;
+blackboard.addEventListener('mouseenter',function(){
+    
+
+})
+
+
+
+
+
